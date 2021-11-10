24c6a63651888445865e9e257ac2824b6458c2db
Cam Brown <cambrown@u.northwestern.edu>
2021-11-10 15:04:31 -0600

    updated register file to fix some ordering

--- src/register_file.v
+++ src/register_file.v
@@ -16,6 +16,14 @@ wire [31:0] output_wires[31:0];
 
 // Note: registers 1, 26 and 27 are reserved for assembler/operating system, but I don't think that means we don't access them in hardware
 
+// In order to make sure writing is valid, just mux_32 with write_enable as the selector
+
+// I think this actually needs to be a decoder?
+// We need to figure out how to map this to the proper input wire index...
+// Essentially just do input_wires[write_reg] = write_data
+
+mux_32 write_enable_mux_(.sel(write_enable), .src0(32'h0), .src1(write_data), .z(input_wires[write_reg]))
+
 // generate the bank of registers
 // setting async reset, load and data to GND/0 until we want to implement that functionality
 genvar i;
@@ -25,16 +33,17 @@ generate
     end
 endgenerate
 
+// this is the zero register, so just putting this after the for loop to ensure 0s are written to it.
+
+assign output_wires[0] = 32'h0;     
+
 // These two muxes handle selecting which registers we are reading from
 
 mux_32t1_32_arr read_sel_mux_1_(.sel(read_reg1), .in_arr(output_wires), .out(read_data1));
 mux_32t1_32_arr read_sel_mux_2_(.sel(read_reg2), .in_arr(output_wires), .out(read_data2));
 
-// In order to make sure writing is valid, just mux_32 with write_enable as the selector
 
-mux_32t1_32_arr write_sel_mux_(.sel(write_reg), .in_arr(write_data), .)
 
-assign output_wires[0] = 32'h0;     // this is the zero register, so just putting this at the end to ensure 0s are written to it.
 
 
 
