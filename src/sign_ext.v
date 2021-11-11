cac0a9ea5e21b8e6498de799a3ed6f835d1c4ab0
Alexandra Lauren Day <ald7177@joker.ece.northwestern.edu>
2021-11-11 13:48:18 -0600

    updated sign extender and test; works

--- src/sign_ext.v
+++ src/sign_ext.v
@@ -1,14 +1,15 @@
 // AD first attempt at sign extender.  Needs to take the sign of the msb and extend to 32 bits from 16.
 
-module sgn_ext(a);
+`timescale 1ns/10ps
+
+module sign_ext(a, a_ext);
    input [15:0] a;
    output wire [31:0] a_ext; // need wire?
-   wire [0:0];
 
    assign a_ext[15:0] = a[15:0];
-   assign a_ext[31:16] = a[15]; // I think this is it...too simple?  Clocking?
+   assign a_ext[31:16] = {16{a[15]}}; // I think this is it...too simple?  Clocking?
 
-endmodule // sgn_ext
+endmodule // sign_ext
 
    
    
