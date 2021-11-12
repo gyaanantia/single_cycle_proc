682b38e6476931c4148742df9a83c92f1070018c
Alexandra Lauren Day <ald7177@joker.ece.northwestern.edu>
2021-11-12 12:13:59 -0600

    edited instruction fetch and added launch directive

--- src/ins_fetch.v
+++ src/ins_fetch.v
@@ -1,21 +1,24 @@
-// AD first attempt at instruction fetch module.  P. 253 in text.
-
 `timescale 1ns/10ps
 
-module ins_fetch(pc_in, ins_out); //input: pc counter value; output: instruction
+// AD first attempt at instruction fetch module.  P. 253 in text.
+
+module ins_fetch(clk, reset, pc_in, ins_out); //input: pc counter value; output: instruction
+
+    // define a clock here
 
     parameter pc_start = 32'h00400020; //this is what we are given, make param for flexibility
     input [31:0] pc_in;
+    input clk, reset;
     output wire [31:0] ins_out;
     wire [31:0] pc_out; // need a wire for the pc result
 
     register pc( // add a register to be the pc.  Just hard-code most of these?
-        .clk(1'b1), 
+        .clk(clk), 
         .areset(1'b0), 
-        .aload(1'b0), 
-        .adata(32'h00000000), //no asynch. data in 
+        .aload(reset), 
+        .adata(pc_start), //reloads initial value when aload asserted
         .data_in(pc_in), 
-        .write_enable(1'b1), // want to be able to write at end 
+        .write_enable(1'b1), // want to be able to write at end, always
         .data_out(pc_out) // wire in ins_fetch module
     );
 
