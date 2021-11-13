7d9c5d9da3c3f08004598d5813f0ae4414584393
Cam Brown <cambrown@u.northwestern.edu>
2021-11-12 18:06:19 -0600

    hopefully working register file

--- src/register_file.v
+++ src/register_file.v
@@ -11,7 +11,7 @@ output wire [31:0] read_data1, read_data2;
 // wire [31:0] $zero, $v0, $v1, $a0, $a1, $a2, $a3, $t0, $t1, $t2, $t3, $t4, $t5, $t6, $t7, $s0, $s1, $s2, $s3, $s4, $s5, $s6, $s7, $t8, $t9, $gp, $sp, $fp, $ra;
 
 // Actually just make wire arrays for input and output into the registers
-wire [31:0] input_wires[31:0];
+// wire [31:0] input_wires[31:0];
 wire [31:0] output_wires[31:0];
 
 // Note: registers 1, 26 and 27 are reserved for assembler/operating system, but I don't think that means we don't access them in hardware
@@ -22,16 +22,22 @@ wire [31:0] output_wires[31:0];
 // We need to figure out how to map this to the proper input wire index...
 // Essentially just do input_wires[write_reg] = write_data
 
-wire [31:0] preserve;
-mux_32t1_32_arr write_preserve_mux_1_(.sel(write_reg), .in_arr(output_wires), .out(preserve));
-mux_32 write_enable_mux_(.sel(write_enable), .src0(preserve), .src1(write_data), .z(input_wires[write_reg]));
+wire [31:0] decoder_out;
+wire [31:0] decoder_and_we;
+dec_5 dec_(.src(write_reg), .z(decoder_out));
+genvar k;
+generate
+    for (k = 0; k < 32; k = k + 1) begin
+        and_gate and_dec_we_(.x(write_enable), .y(decoder_out[k]), .(decoder_and_we[k]));
+    end
+endgenerate
 
 // generate the bank of registers
 // setting async reset, load and data to GND/0 until we want to implement that functionality
 genvar i;
 generate
     for (i = 0; i < 32; i = i + 1) begin
-        register reg_(.clk(clk), .areset(1'b0), .aload(1'b0), .adata(1'b0), .data_in(input_wires[i]), .write_enable(write_enable), .data_out(output_wires[i]));
+        register reg_(.clk(clk), .areset(1'b0), .aload(1'b0), .adata(1'b0), .data_in(write_data), .write_enable(decoder_and_we[i]), .data_out(output_wires[i]));
     end
 endgenerate
 
