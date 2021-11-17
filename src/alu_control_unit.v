46772be38131010bfc8a163bc93c37a0d910d08b
Cam Brown <cambrown@u.northwestern.edu>
2021-11-16 21:24:40 -0600

    fixed alu control unit naming

--- src/alu_control_unit.v
+++ src/alu_control_unit.v
@@ -11,8 +11,8 @@ module alu_control_unit(inst, alu_op, sel);
 
     // create inverted versions of the inputs
     gac_not_gate_6_BHV not_inst_(.x(inst), .z(not_inst));
-    gac_not_gate not_alu_op_0(.x(alu_op[0]), .z(not_alu_op[0]));
-    gac_not_gate not_alu_op_1(.x(alu_op[1]), .z(not_alu_op[1]));
+    gac_not_gate not_inst_0(.x(alu_op[0]), .z(not_alu_op[0]));
+    gac_not_gate not_inst_1(.x(alu_op[1]), .z(not_alu_op[1]));
 
     ///////////////////
     //// AND LAYER ////
@@ -24,47 +24,47 @@ module alu_control_unit(inst, alu_op, sel);
 
     // ADD
     wire add_, add_r;
-    gac_and_gate_6in add__(.a(not_alu_op[0]), .b(not_alu_op[1]), .c(not_alu_op[2]), .d(not_alu_op[3]), .e(not_alu_op[4]), .f(alu_op[5]), .z(add_));
+    gac_and_gate_6in add__(.a(not_inst[0]), .b(not_inst[1]), .c(not_inst[2]), .d(not_inst[3]), .e(not_inst[4]), .f(inst[5]), .z(add_));
     gac_and_gate add_r_(.x(add_), .y(r_type), .z(add_r));
 
     // ADDU
     wire addu_, addu_r;
-    gac_and_gate_6in addu__(.a(alu_op[0]), .b(not_alu_op[1]), .c(not_alu_op[2]), .d(not_alu_op[3]), .e(not_alu_op[4]), .f(alu_op[5]), .z(addu_));
+    gac_and_gate_6in addu__(.a(inst[0]), .b(not_inst[1]), .c(not_inst[2]), .d(not_inst[3]), .e(not_inst[4]), .f(inst[5]), .z(addu_));
     gac_and_gate addu_r_(.x(addu_), .y(r_type), .z(addu_r));
 
     // SUB
     wire sub_, sub_r;
-    gac_and_gate_6in sub__(.a(not_alu_op[0]), .b(alu_op[1]), .c(not_alu_op[2]), .d(not_alu_op[3]), .e(not_alu_op[4]), .f(alu_op[5]), .z(sub_));
+    gac_and_gate_6in sub__(.a(not_inst[0]), .b(inst[1]), .c(not_inst[2]), .d(not_inst[3]), .e(not_inst[4]), .f(inst[5]), .z(sub_));
     gac_and_gate sub_r_(.x(sub_), .y(r_type), .z(sub_r));
 
     // SUBU
     wire subu_, subu_r;
-    gac_and_gate_6in subu__(.a(alu_op[0]), .b(alu_op[1]), .c(not_alu_op[2]), .d(not_alu_op[3]), .e(not_alu_op[4]), .f(alu_op[5]), .z(subu_));
+    gac_and_gate_6in subu__(.a(inst[0]), .b(inst[1]), .c(not_inst[2]), .d(not_inst[3]), .e(not_inst[4]), .f(inst[5]), .z(subu_));
     gac_and_gate subu_r_(.x(subu_), .y(r_type), .z(subu_r));
 
     // AND
     wire and_, and_r;
-    gac_and_gate_6in and__(.a(not_alu_op[0]), .b(not_alu_op[1]), .c(alu_op[2]), .d(not_alu_op[3]), .e(not_alu_op[4]), .f(alu_op[5]), .z(and_));
+    gac_and_gate_6in and__(.a(not_inst[0]), .b(not_inst[1]), .c(inst[2]), .d(not_inst[3]), .e(not_inst[4]), .f(inst[5]), .z(and_));
     gac_and_gate and_r_(.x(and_), .y(r_type), .z(and_r));
 
     // OR
     wire or_, or_r;
-    gac_and_gate_6in or__(.a(alu_op[0]), .b(not_alu_op[1]), .c(alu_op[2]), .d(not_alu_op[3]), .e(not_alu_op[4]), .f(alu_op[5]), .z(or_));
+    gac_and_gate_6in or__(.a(inst[0]), .b(not_inst[1]), .c(inst[2]), .d(not_inst[3]), .e(not_inst[4]), .f(inst[5]), .z(or_));
     gac_and_gate or_r_(.x(or_), .y(r_type), .z(or_r));
 
     // SLL
     wire sll_, sll_r;
-    gac_and_gate_6in sll__(.a(not_alu_op[0]), .b(not_alu_op[1]), .c(not_alu_op[2]), .d(not_alu_op[3]), .e(not_alu_op[4]), .f(not_alu_op[5]), .z(sll_));
+    gac_and_gate_6in sll__(.a(not_inst[0]), .b(not_inst[1]), .c(not_inst[2]), .d(not_inst[3]), .e(not_inst[4]), .f(not_inst[5]), .z(sll_));
     gac_and_gate sll_r_(.x(sll_), .y(r_type), .z(sll_r));
 
     // SLT
     wire slt_, slt_r;
-    gac_and_gate_6in slt__(.a(not_alu_op[0]), .b(alu_op[1]), .c(not_alu_op[2]), .d(alu_op[3]), .e(not_alu_op[4]), .f(alu_op[5]), .z(slt_));
+    gac_and_gate_6in slt__(.a(not_inst[0]), .b(inst[1]), .c(not_inst[2]), .d(inst[3]), .e(not_inst[4]), .f(inst[5]), .z(slt_));
     gac_and_gate slt_r_(.x(slt_), .y(r_type), .z(slt_r));
 
     // SLTU
     wire sltu_, sltu_r;
-    gac_and_gate_6in sltu__(.a(alu_op[0]), .b(alu_op[1]), .c(not_alu_op[2]), .d(not_alu_op[3]), .e(not_alu_op[4]), .f(alu_op[5]), .z(sltu_));
+    gac_and_gate_6in sltu__(.a(inst[0]), .b(inst[1]), .c(not_inst[2]), .d(not_inst[3]), .e(not_inst[4]), .f(inst[5]), .z(sltu_));
     gac_and_gate sltu_r_(.x(sltu_), .y(r_type), .z(sltu_r));
 
     // ADDI
