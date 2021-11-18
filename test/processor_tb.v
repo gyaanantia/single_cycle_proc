bb187c54892bfb1b19f6a7e8e18cbd0e8d4e10d3
gyaanantia <>
2021-11-17 22:02:53 -0600

    something worksgit add test/processor_tb.v

--- test/processor_tb.v
+++ test/processor_tb.v
@@ -4,7 +4,7 @@ module processor_tb;
 
     reg clk_tb;
     reg reset_tb;
-    reg [31:0] load_pc_tb;
+    reg load_pc_tb;
     wire [31:0] z_tb;
 
     processor test(
@@ -18,11 +18,12 @@ module processor_tb;
         clk_tb = 0;
         reset_tb = 1;
         #10
-        load_pc_tb = 32'h00400020;
+        load_pc_tb = 1;
         reset_tb = 0;
         #10
+        load_pc_tb = 0;
         #10
-        #10
+        #500
         $finish;
 
 
